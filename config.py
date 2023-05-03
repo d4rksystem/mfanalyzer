cd5651afbd7fec9129b46ada8aa4f166e6e8bbc1
Kyle Cucci <kyle.cucci@gmail.com>
2023-05-03 16:36:45 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+#################################################
+## This module contains the configs for the rest of the script. 
+## Modify the tool paths and other values only if needed.
+##################################################
+
+# ----- Tool Paths -----
+# Tool paths below are default for Remnux! Change if required!
+
+textEditor = 'gedit'
+file = 'file'
+pescanner = 'pescanner.py'
+portex = 'portex'
+floss = 'floss'
+strings = 'strings'
+xorsearch = 'xorsearch'
+base64dump = 'base64dump.py'
+oledump = 'oledump.py'
+oleid = 'oleid'
+olevba = 'olevba'
+zipdump = 'zipdump.py'
+exif = 'exiftool'
+pdfid = 'pdfid.py'
+pdfparser = 'pdf-parser.py'
+pdfcop = 'pdfcop'
+rtfdump = 'rtfdump.py'
+rtfobj = 'rtfobj'
+peepdf = 'peepdf'
+brxor = 'brxor.py'
+pcodedmp = 'pcodedmp'
+yara = 'yara'
+stringsifter = 'rank_strings'
+malconf  = 'malconf'
+balbuzard = 'balbuzard'
+
+# ----- Configs -----
+
+minStringLength = '8'		# Define the minimum length of characters for string searches
+
+# ----- Global Vars -----
+
+outDir = ''			# Default output directory. Don't change this or bad things will happen :O
